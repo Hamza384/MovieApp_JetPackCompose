c57856b3a95fa8c2f3f6b41b1a9c63fa5dabdf6a
Hamza384 <hamzaciit133@gmail.com>
2022-06-16 01:11:31 -0700

    Navigation Done Commit

--- app/src/main/java/com/example/movieapp_jetpackcompose/NavHostController.kt
+++ app/src/main/java/com/example/movieapp_jetpackcompose/NavHostController.kt
@@ -1,11 +1,27 @@
 package com.example.movieapp_jetpackcompose
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.movieapp_jetpackcompose.utils.NavRoutes
 
 @Composable
-fun NavHostController() {
+fun NavHostController(navController: NavHostController) {
 
+    val navController = rememberNavController()
 
+    NavHost(navController = navController, startDestination = NavRoutes.SplashScreen.routes) {
 
+        composable(NavRoutes.SplashScreen.routes) {
+            SplashScreenView(navController)
+        }
+
+        composable(NavRoutes.MainActivity.routes) {
+            HomeComponent(navController = navController)
+        }
+
+    }
 
 }
--- app/src/main/java/com/example/movieapp_jetpackcompose/SplashActivity.kt
+++ app/src/main/java/com/example/movieapp_jetpackcompose/SplashActivity.kt
@@ -1,2 +1,105 @@
 package com.example.movieapp_jetpackcompose
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.movieapp_jetpackcompose.ui.theme.BackgroundColor
+import com.example.movieapp_jetpackcompose.ui.theme.Poppins
+import com.example.movieapp_jetpackcompose.utils.NavRoutes
+
+
+@Composable
+fun SplashScreenView(navController: NavController) {
+
+
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize(),
+        backgroundColor = BackgroundColor
+    ) {
+        SplashContent(navController)
+    }
+}
+
+
+@Composable
+fun SplashContent(navController: NavController) {
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Bottom
+    ) {
+        ButtonNext(navController)
+    }
+
+}
+
+
+@Composable
+fun ButtonNext(navController: NavController) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(IntrinsicSize.Min),
+        horizontalArrangement = Arrangement.End
+    ) {
+        Card(
+            modifier = Modifier
+                .height(50.dp)
+                .width(150.dp)
+                .padding(4.dp)
+                .clip(RoundedCornerShape(24.dp))
+                .clickable {
+                    navController.navigate(NavRoutes.MainActivity.routes)
+                },
+            backgroundColor = Color(0xFF161A37),
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Start",
+                    style = TextStyle(
+                        color = Color.White,
+                        fontFamily = Poppins,
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.SemiBold,
+                        textAlign = TextAlign.Center
+                    )
+                )
+            }
+        }
+
+    }
+}
+
+@Preview(
+    showBackground = true
+)
+@Composable
+fun showSplash() {
+    SplashScreenView(navController = rememberNavController())
+}
+
